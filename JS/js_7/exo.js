8b330e96c2e2fbdf92e500d1eda2bac538f3e413
phan-htc <phan-htc@outlook.fr>
2022-02-03 20:48:32 +0100

    intégration de correction js

---
+++ JS/js_7/exo.js
@@ -0,0 +1,46 @@
+//exo 1
+let a = [0, 0, 0, 0, 0, 0, 0];
+
+// autre solution ( correction )
+
+let b =[];
+for(let i=0; i<7; i++){
+    b.push(0);
+}
+
+/*=======================================*/ 
+
+// exo 2
+/*Demander un nombre à l'utilisateur puis remplir un tableau contenant les 10
+  valeurs suivantes de cenombre. Afficher ensuite ces valeurs dans la console. */
+
+let ValuesFromUser = [];                                    // déclaration du tableau
+let Val = Number(prompt("entrer un nombre au tableau"))     //demander a l'user d'entrer un nombre
+ValuesFromUser.push(Val);                                   // 
+console.log()
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const values = [0,0,0,0,0,0,0]; // ??
+
+
+
+
+
+var nb = []; // déclare le tableau 
+for ( var i = 0; i<6; i++);
+
+
+// 7.6 aucun fonction admis
+
+*/
