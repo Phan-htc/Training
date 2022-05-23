f62468ebee677a98ab38e267a9451100a6337b9e
Phan-htc <phan-htc@outlook.fr>
2022-05-23 02:05:47 +0200

    ajout du dossier Dart et création du premier fichier codé en dart. Il s'agit d'une leçon avec du code comme exemple.

---
+++ Dart/first-exp.dart
@@ -0,0 +1,31 @@
+void main(){
+    print("Bonjour Mr")
+}
+// /* */ ceci sont les balises de commentaires
+// Comme en PHP, C, JAVA, toutes les commandes en dart finissent avec un (;) .
+// la fonction main suivi d'un void indique que la fonction ne renvoie rien.
+// la parenthèse indique qu'il s'agit d'une fonction et son code est dans les {}. 
+// on peux nommer les variables var, int, if, else ou encore while.
+
+var mySize = 133;
+print(mySize);
+int yourSize = 323;
+print(yourSize);
+double thisSize = 1.44;
+print(thisSize);
+dynamic oneSize = 'Ninethousand' + ': ' + '9000';
+print(oneSize);
+
+// la commande print affiche dans DartPad
+
+void main(){
+    String text1 = 'ceci est une ligne simple';
+    String text2 =''' ceci sont des lignes multiple avec un break ''';
+    pring(text1);
+    print(text2);
+}
+
+// on peux encadrer le contenu de guillemets simples ou double ' " .
+// Si le texte est encadré de 3 guillemets simple ou double (''' / """), il sera 
+// afficher en dart avec les sauts de lignes d'origine.
+
